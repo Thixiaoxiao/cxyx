a733a96a2304b60cb84070a426caaaf116061600
Thixiaoxiao <45098520+Thixiaoxiao@users.noreply.github.com>
2020-07-14 17:14:07 +0800

    Update logger.py

--- cxyx/utils/logger.py
+++ cxyx/utils/logger.py
@@ -1,29 +1,29 @@
-import os
 import logging
 from logging.handlers import RotatingFileHandler
 
+from cxyx.core.config import Config
+
 
 class Log:
-    def __init__(self, name=__name__,
-                 logfile="log_%s.txt"%(os.getpid()),
-                 log_to_console=True,
-                 log_to_file=False
-                 ):
+    def __init__(self, name=__name__, ):
         logger = logging.getLogger(name)
-        logger.setLevel(level=logging.INFO)
+        logger.setLevel(level=getattr(logging, Config.LOG_LEVEL))
         logger.handlers.clear()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(process)d - %(thread)d - %(message)s')
-        if log_to_file:
-            rHandler = RotatingFileHandler(logfile, maxBytes=1 * 1024, backupCount=3)
-            rHandler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(process)d - %(thread)d - %(message)s')
+        if Config.LOG_TO_FILE:
+            rHandler = RotatingFileHandler(Config.LOG_FILE,
+                                           maxBytes=Config.LOG_FILE_MAX_BYTES,
+                                           backupCount=Config.LOG_FILE_BACKUP_COUNT)
+            rHandler.setLevel(getattr(logging, Config.LOG_LEVEL))
             rHandler.setFormatter(formatter)
             logger.addHandler(rHandler)
-        if log_to_console:
+        if Config.LOG_TO_CONSOLE:
             console = logging.StreamHandler()
-            console.setLevel(logging.INFO)
+            console.setLevel(getattr(logging, Config.LOG_LEVEL))
             console.setFormatter(formatter)
             logger.addHandler(console)
-        self.__logger = logger                  
+        self.__logger = logger
 
     @property
     def logger(self):
