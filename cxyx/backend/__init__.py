94b309027a8fad8237433aa668ec161165af7ec5
Thixiaoxiao <45098520+Thixiaoxiao@users.noreply.github.com>
2020-07-15 14:46:19 +0800

    Update __init__.py

--- cxyx/backend/__init__.py
+++ cxyx/backend/__init__.py
@@ -6,7 +6,20 @@ from cxyx.utils.redis_engine import RedisEngine
 
 class Backend:
     def _init(self):
-        if hasattr(Config, "REDIS_BACKEND") and getattr(Config, "REDIS_BACKEND"):
+        if hasattr(Config, "BACKEND_INFO"):
+            redis_info_dict = getattr(Config, "BACKEND_INFO")
+            self._broker_engine = RedisEngine(
+                redis_host=redis_info_dict[
+                    "redis_host"] if "redis_host" in redis_info_dict else Config.REDIS_HOST,
+                redis_port=redis_info_dict[
+                    "redis_port"] if "redis_port" in redis_info_dict else Config.REDIS_PORT,
+                db=redis_info_dict[
+                    "redis_db"] if "redis_db" in redis_info_dict else Config.REDIS_DB,
+                redis_password=redis_info_dict[
+                    "redis_password"] if "redis_password" in redis_info_dict else Config.REDIS_PASSWORD
+            )
+        elif hasattr(Config, "REDIS_BACKEND") and getattr(Config,
+                                                          "REDIS_BACKEND"):
             self._backend_engine = RedisEngine(
                 redis_host=Config.REDIS_HOST,
                 redis_port=Config.REDIS_PORT,
@@ -22,6 +35,5 @@ class Backend:
 
     def save_result(self, key, val):
         val = pickle.dumps(val)
-        self._backend_engine.save_result(key, val, expire_time=Config.RESULT_EXPIRE_TIME)
-
-
+        self._backend_engine.save_result(key, val,
+                                         expire_time=Config.RESULT_EXPIRE_TIME)
