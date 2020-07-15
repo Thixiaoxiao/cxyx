251e06036322c66def0d343b731c1e152228b381
Thixiaoxiao <45098520+Thixiaoxiao@users.noreply.github.com>
2020-07-15 14:43:34 +0800

    Update __init__.py

--- cxyx/broker/__init__.py
+++ cxyx/broker/__init__.py
@@ -4,9 +4,23 @@ import pickle
 import uuid
 import sys
 
+
 class Broker:
     def _init(self):
-        if hasattr(Config, "REDIS_BROKER") and getattr(Config, "REDIS_BROKER"):
+        if hasattr(Config, "BROKER_INFO"):
+            redis_info_dict = getattr(Config, "BROKER_INFO")
+            self._broker_engine = RedisEngine(
+                redis_host=redis_info_dict[
+                    "redis_host"] if "redis_host" in redis_info_dict else Config.REDIS_HOST,
+                redis_port=redis_info_dict[
+                    "redis_port"] if "redis_port" in redis_info_dict else Config.REDIS_PORT,
+                db=redis_info_dict[
+                    "redis_db"] if "redis_db" in redis_info_dict else Config.REDIS_DB,
+                redis_password=redis_info_dict[
+                    "redis_password"] if "redis_password" in redis_info_dict else Config.REDIS_PASSWORD
+            )
+        elif hasattr(Config, "REDIS_BROKER") and getattr(Config,
+                                                         "REDIS_BROKER"):
             self._broker_engine = RedisEngine(
                 redis_host=Config.REDIS_HOST,
                 redis_port=Config.REDIS_PORT,
@@ -24,9 +38,11 @@ class Broker:
         return self._broker_engine
 
     def add_task(self, task_info):
-        task_info["id_"] = str(uuid.uuid1()).replace("-","")
-        self._broker_engine.add_task(Config.BROKER_QUEUE_KEY, pickle.dumps(task_info))
+        task_info["id_"] = str(uuid.uuid1()).replace("-", "")
+        self._broker_engine.add_task(Config.BROKER_QUEUE_KEY,
+                                     pickle.dumps(task_info))
         return task_info["id_"]
+
     def get_task(self):
         taskbyte = self._broker_engine.get_task(Config.BROKER_QUEUE_KEY)
         if taskbyte:
